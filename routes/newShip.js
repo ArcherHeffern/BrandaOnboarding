7554d4f141b0de7290c65c28eadacd1706c1ddb3
Archer <heffernarcher@gmail.com>
2022-10-27 12:37:40 -0400

    added route to query for ship by name and update launched property

--- routes/newShip.js
+++ routes/newShip.js
@@ -72,4 +72,32 @@ router.get('/getShip/:key/:value', (req, res) => {
   })
 })
 
+/*
+Our stored years of launch are not very accurate. While year of design can be fuzzy, a ship's launch date is kind of important. Let's try and update the launch dates of our non-napkin ships to be more accurate. Create a new route that will accept PATCH requests at /updateShip and update the ship with the given name to have whichever new fields are passed in the request body.
+Use the Mongoose findOneAndUpdate function.
+Assume that the name of the ship to update is always included in the request.
+Assume that there will be at least one field to update included in the request.
+If no ship with the given name is found, return HTTP code 404.
+If there is an error while finding the ship to update, return HTTP code 500 and the error message.
+Upon a successful update, return the updated document */
+
+router.patch('/updateShip', (req, res) => {
+  // If no ship name is given in the request, return HTTP code 400.
+  if (!req.body.name) {
+    res.status(400).send('No ship specified')
+  }
+  Ship.findOneAndUpdate({ name: req.body.name }, { launched: req.body.newDate }, { new: true }, (err, doc) => {
+    if (err) {
+      res.status(500).send('Error finding the ship')
+    }
+    else if (!doc) {
+      res.status(404).send('No ship with the given name found')
+    }
+    else {
+      res.status(200).send(doc)
+    }
+  })
+})
+
+
 module.exports = router;
